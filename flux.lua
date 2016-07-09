9c5e9cbe41941f974f38c0d980424332f6c4e39e
rxi <rxi@users.noreply.github.com>
2016-07-09 15:29:35 +0100

    Updated copyright year

--- flux.lua
+++ flux.lua
@@ -1,7 +1,7 @@
 --
 -- flux
 --
--- Copyright (c) 2014, rxi
+-- Copyright (c) 2016 rxi
 --
 -- This library is free software; you can redistribute it and/or modify it
 -- under the terms of the MIT license. See LICENSE for details.
