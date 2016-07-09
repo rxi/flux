bb330231b87eabf84fbd68322f13a6320db30a41
rxi <rxi@users.noreply.github.com>
2016-07-09 15:29:59 +0100

    Version 0.1.5

--- flux.lua
+++ flux.lua
@@ -7,7 +7,7 @@
 -- under the terms of the MIT license. See LICENSE for details.
 --
 
-local flux = { _version = "0.1.4" }
+local flux = { _version = "0.1.5" }
 flux.__index = flux
 
 flux.tweens = {}
